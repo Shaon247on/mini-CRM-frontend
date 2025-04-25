2c002317919e0cbb936eab17d51fc03b181a5aea
Md Aminul Islam Shaon <aminul007.ais@gmail.com>
2025-04-26 04:09:06 +0600

    client table is ready and fully responsive. Need to work on Backend

--- src/features/dashboard/client/Client.tsx
+++ src/features/dashboard/client/Client.tsx
@@ -1,17 +1,67 @@
 import { useDashboard } from "@/provider/DashboardContext";
+import { DataTable } from "./components/data-table";
+import { ClientType, columns } from "./components/columns";
+import { Suspense } from "react";
+import LoadingSpinner from "@/components/LoadingSpinner";
+
+export const mockClients: ClientType[] = [
+  {
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+    phone: "01712345678",
+    company: "Creative Co.",
+    notes: "Interested in a long-term partnership.",
+    creatorId: 1234,
+  },
+  {
+    name: "Michael Smith",
+    email: "michael.smith@example.com",
+    phone: "01898765432",
+    company: "Smith & Sons",
+    notes: "Needs follow-up after Q2.",
+    creatorId: 1234,
+  },
+  {
+    name: "Sarah Lee",
+    email: "sarah.lee@example.com",
+    phone: "01987654321",
+    company: "TechWave Ltd.",
+    notes: "",
+    creatorId: 1234,
+  },
+  {
+    name: "David Kim",
+    email: "david.kim@example.com",
+    phone: "01611223344",
+    company: "Edge Innovations",
+    notes: "Requested a quote for mobile app redesign.",
+    creatorId: 1234,
+  },
+  {
+    name: "Emma Watson",
+    email: "emma.watson@example.com",
+    phone: "01599887766",
+    company: "Watson Ventures",
+    notes: "Prefers email communication.",
+    creatorId: 1234,
+  },
+];
 
 export default function Client() {
   const { isSideBar } = useDashboard();
+
   return (
     <div className="flex justify-end">
       <h1
-        className={`
-    border-2 h-full border-red-800
-    transition-all duration-700 ease-in-out
-    ${isSideBar ? "w-[82.5%]" : "w-full"}
-  `}
+        className={`h-full transition-all duration-700 ease-in-out
+          ${isSideBar ? "w-full md:w-[70%] lg:w-[82.5%]" : "w-full"}
+        `}
       >
-        Client
+        <Suspense fallback={<LoadingSpinner />}>
+            <div className="m-5">
+              <DataTable<ClientType, unknown> data={mockClients} columns={columns} />
+            </div>        
+        </Suspense>
       </h1>
     </div>
   );
---
+++ src/features/dashboard/client/components/clientSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const clientSchema = z.object({
+  id: z.number().optional(), // ✅ Optional ID for update cases
+  name: z.string().min(1, { message: "Client name is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  phone: z
+    .string()
+    .min(11, { message: "Phone number must be at least 11 digits" }),
+  company: z.string().optional(),
+  notes: z.string().optional(),
+  creatorId: z.number().int({ message: "Creator ID must be an integer" }).min(1, { message: "Creator ID is required" }), // creatorId as a number
+});
